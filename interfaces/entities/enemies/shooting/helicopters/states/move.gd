1aeed0c44a9121c9261ce5dfabc11624c8a23a4d
Gayathri Mounika <78194923+Gayathri0616@users.noreply.github.com>
2025-05-18 10:15:14 -0700

    Delete interfaces directory

--- interfaces/entities/enemies/shooting/helicopters/states/move.gd
+++
@@ -1,8 +0,0 @@
-extends "res://entities/enemies/states/move.gd"
-
-
-func update(delta: float) -> void:
-	super(delta)
-	# check if we need to exit the state
-	if (owner as ShootingEnemy).shooter.targets.size() > 0:
-		finished.emit("shoot_still")
--- interfaces/entities/enemies/shooting/tanks/states/die.gd
+++
@@ -1,6 +0,0 @@
-extends "res://entities/enemies/shooting/states/die.gd"
-
-
-func enter() -> void:
-	super()
-	(owner as Tank).explosion.play("default")
