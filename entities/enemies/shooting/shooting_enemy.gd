834ab39083839b72e6e12ad3ebbb8f12bf2a74cc
Gayathri Mounika <78194923+Gayathri0616@users.noreply.github.com>
2025-05-18 10:15:04 -0700

    Delete entities directory

--- entities/enemies/shooting/shooting_enemy.gd
+++
@@ -1,9 +0,0 @@
-class_name ShootingEnemy
-extends Enemy
-
-
-@onready var shooter := $Shooter as Shooter
-
-
-func _on_shooter_has_shot(reload_time: float) -> void:
-	hud.update_reloadbar(reload_time)
--- entities/enemies/shooting/tanks/tank.gd
+++
@@ -1,5 +0,0 @@
-class_name Tank
-extends ShootingEnemy
-
-
-@onready var explosion := $Explosion as AnimatedSprite2D
